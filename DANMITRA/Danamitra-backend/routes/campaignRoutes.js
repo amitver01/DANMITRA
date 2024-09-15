0c51f02493c937c9578ec08e98b8b598461fefb9
AMIT VERMA <113676945+amitver01@users.noreply.github.com>
2024-09-15 23:36:38 +0530

    Delete DANMITRA/Danamitra-backend directory

--- DANMITRA/Danamitra-backend/routes/campaignRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createCampaign, deleteCampaign , campaign } = require('../controllers/campaignController');
-
-router.post('/create', createCampaign);
-router.post('/delete', deleteCampaign);
-router.get('/campaign', campaign);
-
-module.exports = router;
--- DANMITRA/Danamitra-backend/routes/orgRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerOrg, loginOrg } = require('../controllers/orgController');
-
-router.post('/register', registerOrg);
-router.post('/login', loginOrg);
-
-module.exports = router;
